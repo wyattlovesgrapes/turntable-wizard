4fd05d5857007c54b34ff106863fbe76801d8539
wyattlovesgrapes <wyattlovesgrapes@gmail.com>
2025-02-26 15:44:08 -0800

    capitalized App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import './app.css'
+import './App.css'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import GIF from 'gif.js'
